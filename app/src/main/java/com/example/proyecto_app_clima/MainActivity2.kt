a8ea92ba9815ae450063b388758d37d27ab0cefd
Derick96 <60660484+Derick96@users.noreply.github.com>
2020-10-30 10:37:35 -0600

    Correción de error pantalla2 y funcionamiento correcto de las 3 pantallas para ir de una a otra.

---
+++ app/src/main/java/com/example/proyecto_app_clima/MainActivity2.kt
@@ -0,0 +1,21 @@
+package com.example.proyecto_app_clima
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+    fun MainActivity3(view: View) {
+        val cambio = Intent(this, MainActivity3()::class.java)
+        startActivity(cambio)
+    }
+    fun MainActivity(view: View) {
+        val cambio = Intent(this, MainActivity()::class.java)
+        startActivity(cambio)
+    }
+}
